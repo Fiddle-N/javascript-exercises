e69415f5a9ebd9fe5488e8fc01eb8796a88ee0b3
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-14 17:29:52 +0000

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const strArray = [];
 
+    for (const char of string) {
+        strArray.push(char);
+    }
+
+    const reversedArray = strArray.reverse();
+
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
