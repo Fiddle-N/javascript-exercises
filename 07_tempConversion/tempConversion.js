82ec8413318d5e6e859571531d2f79fe2af4e9e7
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-14 23:16:08 +0000

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundDp = function (num, dp) {
+  return Number(Math.round(num + 'e' + dp) + 'e-' + dp);
+}
+
+const convertToCelsius = function(fah) {
+  return roundDp((fah - 32) * (5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return roundDp(cel * (9 / 5) + 32, 1);
 };
 
 // Do not edit below this line
