2b40c37875b6d7432ed2b134bca949d3c95bf947
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-14 22:18:24 +0000

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    for (const num of [num1, num2]) {
+        if (typeof(num) != 'number' || num < 0 ) {
+            return 'ERROR';
+        }
+    }
 
+    let start;
+    let end;
+
+    if (num1 <= num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        start = num2;
+        end = num1;
+    }
+
+    let sumVal = 0;
+    for (i = start; i <= end; i++) {
+        sumVal += i;
+    }
+    return sumVal;
 };
 
 // Do not edit below this line
