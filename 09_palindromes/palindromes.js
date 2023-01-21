28fd908531a1ac2d849f8c949d39e55d0413a9e6
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-21 22:58:17 +0000

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    const arr = str.split('');
+    const filteredArr = [];
+    for (const char of arr) {
+        const lowerChar = char.toLowerCase();
+        const lowerCharCode = lowerChar.charCodeAt();
+        if (lowerCharCode >= 97 && lowerCharCode <= 122) {
+            filteredArr.push(lowerChar);
+        }
+    }
+    const reversedFilteredArr = [...filteredArr].reverse();
+    return filteredArr.toString() === reversedFilteredArr.toString();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
