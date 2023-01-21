f341b0cb89484565c45c8a643a44930ea6a5b7b1
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-21 23:29:02 +0000

    Simplify factorial recursion algo

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,12 +24,7 @@ const factorial = function(num) {
 	if (num === 0) {
     return 1;
   }
-  if (num === 1) {
-    return 1;
-  }
-  if (num > 1) {
-    return num * factorial(num - 1);
-  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
