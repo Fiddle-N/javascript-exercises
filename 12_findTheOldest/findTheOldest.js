7c450be69801a22f75b2456a9866b52e6ceac9c2
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-21 23:24:51 +0000

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    currentYear = (person.yearOfDeath) 
+        ? person.yearOfDeath
+        : (new Date()).getFullYear()
+    
+    return currentYear - person.yearOfBirth; 
+}
 
+const sortAgeDesc = function(p1, p2) {
+    const age1 = getAge(p1);
+    const age2 = getAge(p2);
+    return (age1 > age2) ? -1 : 1;
+}
+
+const findTheOldest = function(people) {
+    return [...people]
+        .sort(sortAgeDesc)
+        [0];
 };
 
 // Do not edit below this line
